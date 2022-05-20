044ead22cf9083891df1dc60793e9975643b18b9
PaulNelsonOld <90969624+comcel1@users.noreply.github.com>
2022-05-20 12:46:31 -0500

    added thoughts and reactions to app

--- client/src/App.js
+++ client/src/App.js
@@ -10,10 +10,18 @@ import {
   createHttpLink,
 } from '@apollo/client';
 
+// import page components
 import Header from './components/Header';
 import Footer from './components/Footer';
-
 import Home from './pages/Home';
+import Login from './pages/Login';
+import NoMatch from './pages/NoMatch';
+import SingleThought from './pages/SingleThought';
+import Profile from './pages/Profile';
+import Signup from './pages/Signup';
+
+//BrowserRouter, Routes, and Route are components that the React Router library provides. We renamed BrowserRouter to Router to make it easier to work with.
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 // establish a new link to the GraphQL server
 const httpLink = createHttpLink({
@@ -29,13 +37,22 @@ const client = new ApolloClient({
 function App() {
   return (
     <ApolloProvider client={client}>
-      <div className="flex-column justify-flex-start min-100-vh">
-        <Header />
-        <div className="container">
-          <Home />
+      <Router>
+        <div className="flex-column justify-flex-start min-100-vh">
+          <Header />
+          <div className="container">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/signup" element={<Signup />} />
+              <Route path="/profile" element={<Profile />} />
+              <Route path="/thought/:id" element={<SingleThought />} />
+              <Route path="*" element={<NoMatch />} />
+            </Routes>
+          </div>
+          <Footer />
         </div>
-        <Footer />
-      </div>
+      </Router>
     </ApolloProvider>
   );
 }
